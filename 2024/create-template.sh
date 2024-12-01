bf875e335c18e8c5ea431cee34084160e9c8deb0
johnykosmos <kumakpospolity@gmail.com>
2024-12-01 23:02:45 +0100

    Feat: Create template script for fetching data

---
+++ 2024/create-template.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+SESSION=$(cat session.txt)
+YEAR=2024
+
+mkdir $1 
+
+curl -s -b "session=$SESSION" "https://adventofcode.com/$YEAR/day/$1/input" -o $1/input.txt
+
+touch $1/solve.go
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/johnykosmos/advent-of-code.git
+
+go 1.23.3
