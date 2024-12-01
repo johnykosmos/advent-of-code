573a26207f63fab42b88abdaea95440d2599cb95
johnykosmos <kumakpospolity@gmail.com>
2024-12-02 00:41:58 +0100

    Day 1: Part Two

--- 2024/1/solve.go
+++ 2024/1/solve.go
@@ -44,7 +44,7 @@ func part1(input string) int {
     
     var totalDistance int = 0
     for i := 0; i < len(leftList); i++ {
-        distance := leftList[i] - rightList[i]
+        var distance int = leftList[i] - rightList[i]
         if distance < 0 {
             distance *= -1
         }
@@ -52,6 +52,28 @@ func part1(input string) int {
     }
 
     return totalDistance
+}
+
+func countOccurance(targetNumber int, rightList []int) int {
+    var occurance int
+    for i := 0; i < len(rightList); i++ {
+        if targetNumber == rightList[i]{
+            occurance++
+        }
+    }
+    return occurance
+}
+
+func part2(input string) int {
+    leftList, rightList := parseInput(input)
+
+    var similarityScore int = 0
+    for i := 0; i < len(leftList); i++ {
+        var distance int = leftList[i] * countOccurance(leftList[i], rightList)
+        similarityScore += distance
+    }
+
+    return similarityScore
 }
 
 func getInput() string{
@@ -68,4 +90,5 @@ func main(){
     var input string = getInput()
     
     fmt.Println("The solution for part 1 is:", part1(input))
+    fmt.Println("The solution for part 2 is:", part2(input))
 }
