e5d9e7094065ac253941a192c688d4a1539086b6
johnykosmos <kumakpospolity@gmail.com>
2024-12-12 15:49:31 +0100

    Day 9: Part Two

--- 2024/9/solve.go
+++ 2024/9/solve.go
@@ -60,8 +60,58 @@ func part1(input string) int{
     return filesystemSum 
 }
 
+func getFileSize(spaceLayout []int, startIdx int, endIdx int) int {
+    size := 0
+    step := 1 
+    if endIdx < startIdx {
+        step = -step 
+    }
+    for i := startIdx; i != endIdx && spaceLayout[i] == spaceLayout[startIdx]; i += step {
+		size++
+	}    
+    return size
+}
+
+
+func compactWholeFiles(spaceLayout []int) {
+    fileSize := 0 
+    for i := len(spaceLayout) - 1; i > 0; {
+        if spaceLayout[i] != -1 {
+            fileSize = getFileSize(spaceLayout, i, 0) 
+            for j := 0; j < i;{
+                if spaceLayout[j] == -1 {
+                    freeSpaceSize := getFileSize(spaceLayout, j, i)
+                    if freeSpaceSize >= fileSize {
+                        for k := 0; k < fileSize; k++ {
+                            spaceLayout[j+k] = spaceLayout[i-k]
+                            spaceLayout[i-k] = -1
+                        }
+                        break
+                    } else {
+                        j += freeSpaceSize
+                    }
+                }else {
+                    j++
+                }
+            }
+            i -= fileSize
+        }else {
+            i--
+        }
+    }
+}
+
 func part2(input string) int{
-    return 0
+    filesystemSum := 0
+    spaceLayout := parseInput(input)
+    compactWholeFiles(spaceLayout)
+    for position, fileIndex := range spaceLayout {
+        if fileIndex == -1 {
+           continue 
+        }
+        filesystemSum += position * fileIndex 
+    }
+    return filesystemSum 
 }
 
 func getInput() string{
