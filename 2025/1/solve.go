2952184ca1ff2cdb7a4327526526204bc7b668a9
johnykosmos <piotrpomaranski97@gmail.com>
2025-12-08 17:21:16 +0100

    Day 1 Part 1

---
+++ 2025/1/solve.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "log"
+    "time"
+	"strings"
+	"strconv"
+)
+
+const MAXDIAL int = 100 
+
+func parseInput(input string) []int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	rotationTable := make([]int, len(lines))
+	for lineid, line := range lines {
+		direction := line[0]	
+		rotation, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		if (direction == 'L') {
+			rotation *= -1
+		}
+		rotationTable[lineid] = rotation
+	}
+	return rotationTable
+}
+
+func part1(input string) int{
+	rotationTable := parseInput(input)
+	dial := 50
+	count := 0
+	for _, rotation := range rotationTable {
+		rotatedDial := dial + rotation
+		if (rotatedDial < 0) {
+			dial = (MAXDIAL + rotatedDial) % MAXDIAL
+		} else {
+			dial = rotatedDial % MAXDIAL
+		}
+		fmt.Println(dial)
+		if (dial == 0) {
+			count++
+		}
+	}
+    return count
+}
+
+func part2(input string) int{
+    return 0
+}
+
+func getInput() string{
+    content, err := os.ReadFile("sample-input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    return string(content)
+}
+
+
+func main(){
+    var input string = getInput()
+    
+    start := time.Now()
+    fmt.Printf("The solution for part 1 is: %d\nIn time: %s\n", part1(input), time.Since(start))
+    start = time.Now()
+    fmt.Printf("The solution for part 2 is: %d\nIn time: %s\n", part2(input), time.Since(start))
+}
