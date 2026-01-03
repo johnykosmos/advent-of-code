f424392a0132c13757d0204295fc2610d2f05d6c
johnykosmos <piotrpomaranski97@gmail.com>
2026-01-03 14:37:32 +0100

    Day 5: Part 1

---
+++ 2025/5/solve.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Range struct {
+	start int
+	end int
+}
+
+func parseInput(input string) ([]Range, []int) {
+	splitInput := strings.Split(input, "\n\n")
+	strRanges := strings.Split(splitInput[0], "\n")
+	strNums := strings.Split(strings.Trim(splitInput[1], "\n"), "\n")
+	ranges := make([]Range, len(strRanges))
+	for rangeId, rangeLine := range strRanges {
+		splitRange := strings.Split(rangeLine, "-")
+		startNum, err := strconv.Atoi(splitRange[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		endNum, err := strconv.Atoi(splitRange[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		ranges[rangeId] = Range{start: startNum, end: endNum}
+	}
+
+	numbers := make([]int, len(strNums))
+	for numId, numLine := range strNums {
+		num, err := strconv.Atoi(numLine)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers[numId] = num
+	}
+	return ranges, numbers
+}
+
+
+func part1(input string) int{
+	freshCount := 0
+	ranges, numbers := parseInput(input)
+	fmt.Println(ranges, "\n", numbers)
+	
+	for _, number := range numbers {
+		for _, freshRange := range ranges {
+			if freshRange.start <= number && number <= freshRange.end {
+				freshCount++
+				break
+			}
+		}
+	}
+
+    return freshCount
+}
+
+func part2(input string) int{
+    return 0
+}
+
+func getInput() string{
+    content, err := os.ReadFile("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    return string(content)
+}
+
+
+func main(){
+    var input string = getInput()
+    
+    start := time.Now()
+    fmt.Printf("The solution for part 1 is: %d\nIn time: %s\n", part1(input), time.Since(start))
+    start = time.Now()
+    fmt.Printf("The solution for part 2 is: %d\nIn time: %s\n", part2(input), time.Since(start))
+}
