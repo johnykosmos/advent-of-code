998640bf2f91e874f44a6432f27ecaff5d614838
johnykosmos <piotrpomaranski97@gmail.com>
2026-01-04 23:34:50 +0100

    Day 5: Part 2

--- 2025/5/solve.go
+++ 2025/5/solve.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -47,8 +48,7 @@ func parseInput(input string) ([]Range, []int) {
 func part1(input string) int{
 	freshCount := 0
 	ranges, numbers := parseInput(input)
-	fmt.Println(ranges, "\n", numbers)
-	
+
 	for _, number := range numbers {
 		for _, freshRange := range ranges {
 			if freshRange.start <= number && number <= freshRange.end {
@@ -57,12 +57,32 @@ func part1(input string) int{
 			}
 		}
 	}
-
     return freshCount
 }
 
 func part2(input string) int{
-    return 0
+	freshIdCount := 0
+	ranges, _ := parseInput(input)
+	sort.Slice(ranges, func(i, j int) bool {
+		if ranges[i].start == ranges[j].start {
+			return ranges[i].end < ranges[j].end
+		}
+		return ranges[i].start < ranges[j].start
+	})
+
+	lastRange := ranges[0]
+	freshIdCount += lastRange.end - lastRange.start + 1
+	for i := 1; i < len(ranges); i++ {
+		if lastRange.end >= ranges[i].start {
+			maxEnd := max(lastRange.end, ranges[i].end)
+			freshIdCount += maxEnd - lastRange.end
+			lastRange.end = maxEnd
+		} else {
+			lastRange = ranges[i]
+			freshIdCount += lastRange.end - lastRange.start + 1
+		}
+	}
+    return freshIdCount
 }
 
 func getInput() string{
